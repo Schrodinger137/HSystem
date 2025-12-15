bf34f612bf26d22c68330df202bcb9517b122ee1
Schrodinger137 <elingrato7@gmail.com>
2025-12-14 18:15:53 -0600

    todo

--- products/views.py
+++ products/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def products(request):
-    return render(request, 'products/products.html')
+    products = Product.objects.all()
+    
+    context = {
+        'products':products
+    }
+    return render(request, 'products/products.html', context)
